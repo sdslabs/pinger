d05043afc4f3b9066ed2bf0ac5ab619f54fc7acf
Deepesh Pathak <deepshpathak@gmail.com>
2019-09-01 05:02:58 +0530

    check: add http prober executor to HTTP Checker
    Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

--- pkg/check/http.go
+++ pkg/check/http.go
@@ -1,14 +1,17 @@
 package check
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/sdslabs/status/pkg/api/agent/proto"
 	"github.com/sdslabs/status/pkg/defaults"
+	"github.com/sdslabs/status/pkg/probes"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -24,6 +27,8 @@ type HTTPChecker struct {
 	Headers map[string]string
 
 	HTTPOutput *CheckComponent
+
+	Timeout time.Duration
 }
 
 func NewHTTPChecker(agentCheck *proto.Check) (*HTTPChecker, error) {
@@ -62,11 +67,48 @@ func NewHTTPChecker(agentCheck *proto.Check) (*HTTPChecker, error) {
 			Type:  agentCheck.Output.Type,
 			Value: agentCheck.Output.Value,
 		},
+
+		Timeout: defaults.DefaultHTTPProbeTimeout,
 	}, nil
 }
 
 func (c *HTTPChecker) ExecuteCheck(ctx context.Context) error {
 	log.Debug("Executing HTTP check.")
+	prober := probes.NewHTTPProber()
+
+	result, err := prober.Probe(c.Method, c.URL, c.Headers, c.Payload, c.Timeout)
+	if err != nil {
+		return fmt.Errorf("HTTP Probe error: %s", err)
+	}
+
+	switch c.HTTPOutput.Type {
+	case "status_code":
+		reqStatusCode, _ := strconv.Atoi(c.HTTPOutput.Value)
+		if result.StatusCode == reqStatusCode {
+			log.Info("Check successful")
+		} else {
+			log.Warnf("Check unsuccessful, status(req %d) does not match with %d", reqStatusCode, result.StatusCode)
+		}
+
+	case "body":
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(result.Body)
+		if c.HTTPOutput.Value == buf.String() {
+			log.Info("Check Successful")
+		} else {
+			log.Warnf("Check Unsuccessful")
+		}
+
+	case "header":
+		kv := strings.SplitN(c.HTTPOutput.Value, HEADER_DELIMITER, 2)
+
+		if kv[1] == result.Headers.Get(kv[0]) {
+			log.Info("Check Successful")
+		} else {
+			log.Warn("Check Unsuccessful")
+		}
+	}
+
 	return nil
 }
 
@@ -80,8 +122,8 @@ var validHttpOutputTypes map[string]validationFunction = map[string]validationFu
 
 func validateStatusCode(val string) error {
 	intVal, err := strconv.Atoi(val)
-	if err == nil {
-		return fmt.Errorf("status code prvoided is parsable: %s", err)
+	if err != nil {
+		return fmt.Errorf("status code prvoided is not parsable: %s", err)
 	}
 	if intVal > 599 || intVal < 100 {
 		return fmt.Errorf("status code is not valid(expected between: 100 - 599) got %s")
@@ -128,7 +170,7 @@ func validateHTTPCheck(agentCheck *proto.Check) error {
 
 func validateHTTPInput(input *proto.Check_Component) error {
 	inputVal := input.Value
-	if inputVal != "GET" || inputVal != "POST" || inputVal != "" {
+	if inputVal != "GET" && inputVal != "POST" && inputVal != "" {
 		return fmt.Errorf("for HTTP input the provided method(%s) is not supported", inputVal)
 	}
 
@@ -157,7 +199,7 @@ func validateHTTPTarget(target *proto.Check_Component) error {
 
 func validateHTTPPayload(payloads []*proto.Check_Payloads) error {
 	for _, payload := range payloads {
-		if payload.PayloadType == "header" || payload.PayloadType == "parameter" {
+		if payload.PayloadType == "header" && payload.PayloadType == "parameter" {
 			err := validateKVPair(payload.Payload)
 
 			if err != nil {
--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -5,17 +5,24 @@ import (
 )
 
 var (
-	// Minimum value of the retry interval that the controller accepts.
-	MinControllerRetryInterval time.Duration = time.Second * 4
+	// MinControllerRetryInterval is the minimum value of the retry interval that the controller accepts.
+	MinControllerRetryInterval = time.Second * 4
 
 	// ControllerRetryInterval is the default value of the retry interval used by
 	// the controller.
-	ControllerRetryInterval time.Duration = time.Second * 30
+	ControllerRetryInterval = time.Second * 30
 
 	// MinControllerTimeout is the minimum value of the permissible timeout
 	// that the controller accepts.
-	MinControllerTimeout time.Duration = time.Second * 5
+	MinControllerTimeout = time.Second * 5
 
 	// DefaultHTTPMethod is the default method to use for HTTP Input
 	DefaultHTTPMethod = "GET"
+
+	// DefaultGRPCRequestTimeout is the default timeout for the GRPC request we
+	// are making between the server and the agent and vice versa.
+	DefaultGRPCRequestTimeout = time.Second * 10
+
+	// DefaultHTTPProbeTimeout is the http request timeout for HTTP prober.
+	DefaultHTTPProbeTimeout = time.Second * 10
 )
