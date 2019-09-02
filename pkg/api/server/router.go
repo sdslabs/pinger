16a9bad9dd1eb8a2b2656c5bd34c8ec69fa6f025
Vaibhav <vrongmeal@gmail.com>
2019-09-03 03:00:59 +0530

    api: Shift main api package from root to pkg/api/server.
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- pkg/api/server/router.go
+++ pkg/api/server/router.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sdslabs/status/api/oauth"
+	"github.com/sdslabs/status/pkg/api/server/oauth"
 )
 
 // NewRouter returns the router for the main API service
