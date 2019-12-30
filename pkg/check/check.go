00235363adfd0f99dffc472196ebfef612a96a26
Vaibhav <vrongmeal@gmail.com>
2019-12-30 14:23:51 +0530

    Refactor `pkg/check/check.go`.
    * Add comments to exported fields.
    * Remove validation since it's included in each
      "NewChecker" function.
    
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- pkg/check/check.go
+++ pkg/check/check.go
@@ -2,50 +2,13 @@ package check
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/sdslabs/status/pkg/config"
 	"github.com/sdslabs/status/pkg/controller"
-	"github.com/sdslabs/status/pkg/defaults"
 )
 
-// Checker is the main Check interface we are going to use for the status page.
-// Each check must implment this interface.
-type Checker interface {
-	// This function executes the check for the provided checker.
-	ExecuteCheck(context.Context) (controller.ControllerFunctionResult, error)
-
-	// Returns the type of the checker, this can be http, icmp, websockets etc.
-	Type() string
-}
-
-type CheckStats struct {
-	Successful bool
-
-	StartTime time.Time
-	Duration  time.Duration
-}
-
-func (cd CheckStats) GetDuration() time.Duration {
-	return cd.Duration
-}
-
-func (cd CheckStats) GetStartTime() time.Time {
-	return cd.StartTime
-}
-
-func (cd CheckStats) IsSuccessful() bool {
-	return cd.Successful
-}
-
-type TVComponent struct {
-	Type  string
-	Value string
-}
-
-type InputType string
-
+// Types of inputs accepted for checks.
 const (
 	HTTPInputType      InputType = "HTTP"
 	TCPInputType       InputType = "TCP"
@@ -57,46 +20,65 @@ const (
 // This first validates the agent Check provided in the argument and if the Check
 // is validated then returns a new Checker instance for the provided Check configuration.
 func NewChecker(agentCheck config.Check) (Checker, error) {
-	err := validateCheck(agentCheck)
-	if err != nil {
-		return nil, fmt.Errorf("Error while validating check: %s", err)
-	}
-
 	switch InputType(agentCheck.GetInput().GetType()) {
 	case HTTPInputType:
 		return NewHTTPChecker(agentCheck)
 	case TCPInputType:
 	case WebsocketInputType:
 	case ICMPInputType:
+		return NewICMPChecker(agentCheck)
 	}
 
 	return nil, nil
 }
 
-// Validates the agent check provided in the argument, returns an error
-// if the provided check is not valid.
-func validateCheck(agentCheck config.Check) error {
-	if agentCheck.GetInput() == nil || agentCheck.GetOutput() == nil || agentCheck.GetTarget() == nil {
-		return fmt.Errorf("Input, Ouput and Target are required for the check")
-	}
+// InputType represents the kind of check.
+// Can be HTTP, ICMP, Websocket etc.
+type InputType string
 
-	if agentCheck.GetInterval() < int64(defaults.MinControllerRetryInterval.Seconds()) {
-		return fmt.Errorf("interval provided is less than the minimum value allowed controller retry interval")
-	}
+// Checker is the main Check interface we are going to use for the status page.
+// Each check must implment this interface.
+type Checker interface {
+	// ExecuteCheck executes the check for the provided checker.
+	ExecuteCheck(context.Context) (controller.ControllerFunctionResult, error)
 
-	if agentCheck.GetTimeout() < int64(defaults.MinControllerTimeout.Seconds()) {
-		return fmt.Errorf("timeout for the check is less than the minimum value allowed for controller timeout")
-	}
+	// Type returns the type of the checker, this can be http, icmp, websockets etc.
+	Type() string
+}
 
-	switch InputType(agentCheck.GetInput().GetType()) {
-	case HTTPInputType:
-		return validateHTTPCheck(agentCheck)
-	case TCPInputType:
-	case WebsocketInputType:
-	case ICMPInputType:
-	default:
-		return fmt.Errorf("provided input type is not valid: %s", agentCheck.GetInput().GetType())
-	}
+// TVComponent is a key-value pair component with it's key as the type of component.
+// This is used to represent payloads or any other type-value pair.
+//
+// Example of header payload:
+//     TVComponent{
+//	       Type:  "header",
+//         Value: "Authorizaton=Bearer xyz"
+//     }
+type TVComponent struct {
+	Type  string
+	Value string
+}
 
-	return fmt.Errorf("not a valid input type")
+// CheckStats is the struct with concerned statistics collected by a check.
+type CheckStats struct {
+	Successful bool
+
+	StartTime time.Time
+	Duration  time.Duration
+}
+
+// GetDuration returns the duration taken by check to execute.
+func (cd CheckStats) GetDuration() time.Duration {
+	return cd.Duration
+}
+
+// GetStartTime returns the time when the check started to execute.
+func (cd CheckStats) GetStartTime() time.Time {
+	return cd.StartTime
+}
+
+// IsSuccessful returns true the check executed with desired output
+// or false when the check failed to produce the required output.
+func (cd CheckStats) IsSuccessful() bool {
+	return cd.Successful
 }
