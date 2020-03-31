010a9b61c7186499a09b0f87fb523ae5057389ce
burnerlee <avi.aviral140@gmail.com>
2020-03-31 15:37:15 +0530

    Add TCP Probe.

--- pkg/probes/init.go
+++ pkg/probes/init.go
@@ -1,3 +1,11 @@
 // Package probes defines multiple probes used in the various kinds of checks
 // that can be deployed. These can be ICMP, HTTP, Websocket etc.
 package probes
+
+import "net"
+
+// errIsTimeout returns true for a timeout error.
+func errIsTimeout(err error) bool {
+	netErr, ok := err.(net.Error)
+	return ok && netErr.Timeout()
+}
