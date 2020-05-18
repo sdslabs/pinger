add952242b6525f2d75f8a6309d4925657b8b6d9
Nupur Agrawal <nupur202000@gmail.com>
2020-05-18 21:36:20 +0530

    Adds image and container's inspection methods

--- pkg/api/central/docker.go
+++ pkg/api/central/docker.go
@@ -2,7 +2,10 @@ package central
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"os"
 	"time"
 
 	"github.com/docker/docker/api/types"
@@ -66,16 +69,122 @@ func (cli DockerClient) CreateAndStartContainer(ctx context.Context, containerCo
 		return "", err
 	}
 	containerID := createdConfig.ID
-	if err := cli.c.ContainerStart(ctx, containerID, types.ContainerStartOptions{}); err != nil {
+
+	err = cli.c.ContainerStart(ctx, containerID, types.ContainerStartOptions{})
+	if err != nil {
 		return "", err
 	}
+
 	return containerID, nil
 }
 
 // StopAndRemoveContainer stops and removes the container from the docker host
 func (cli DockerClient) StopAndRemoveContainer(ctx context.Context, containerID string) error {
-	return cli.c.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{
-		RemoveLinks: false,
-		Force:       true,
+	inspectVal, _, err := cli.c.ContainerInspectWithRaw(ctx, containerID, false)
+	if err != nil {
+		return err
+	}
+
+	if inspectVal.ID != "" {
+		err = cli.c.ContainerStop(ctx, containerID, nil)
+		if err != nil {
+			return err
+		}
+
+		err = cli.c.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{Force: true})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// InspectContainer returns container information by using containerID
+func (cli DockerClient) InspectContainer(ctx context.Context, containerID string) (*types.ContainerState, error) {
+	containerStatus, err := cli.c.ContainerInspect(ctx, containerID)
+	if err != nil {
+		return nil, err
+	}
+	return containerStatus.ContainerJSONBase.State, nil
+}
+
+// ListContainers returns a list of containers ids
+func (cli DockerClient) ListContainers(ctx context.Context) ([]string, error) {
+	containers, err := cli.c.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	list := make([]string, 1)
+
+	for _, container := range containers {
+		list = append(list, container.ID)
+	}
+	return list, nil
+}
+
+// ReadLogs returns the logs from a docker container
+func (cli DockerClient) ReadLogs(ctx context.Context, containerID, tail string) ([]string, error) {
+	reader, err := cli.c.ContainerLogs(ctx, containerID, types.ContainerLogsOptions{
+		ShowStderr: true,
+		ShowStdout: true,
+		Timestamps: true,
+		Tail:       tail,
 	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer reader.Close()
+
+	logs := []string{}
+	hdr := make([]byte, 8)
+
+	for {
+		_, err = reader.Read(hdr)
+
+		if err != nil {
+			return logs, err
+		}
+
+		count := binary.BigEndian.Uint32(hdr[4:])
+		dat := make([]byte, count)
+		_, err = reader.Read(dat)
+		logs = append(logs, string(dat))
+
+		if err != nil {
+			return logs, err
+		}
+	}
+}
+
+// PullImage requests the host to pull an image
+func (cli DockerClient) PullImage(ctx context.Context, image string) error {
+	out, err := cli.c.ImagePull(ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	io.Copy(os.Stdout, out)
+	return nil
+}
+
+// BuildImage sends request to build image
+func (cli DockerClient) BuildImage(ctx context.Context)
+
+// RemoveImage removes an image by imageID
+func (cli DockerClient) RemoveImage(ctx context.Context, imageID string) error {
+	inspectVal, _, err := cli.c.ImageInspectWithRaw(ctx, imageID)
+	if err != nil {
+		return err
+	}
+
+	if inspectVal.ID != "" {
+		_, err = cli.c.ImageRemove(context.Background(), imageID, types.ImageRemoveOptions{})
+	}
+
+	return nil
 }
