8399a466510403b1f53a81a0df8cac1f8fd502f6
Vaibhav <vrongmeal@gmail.com>
2020-05-24 17:27:41 +0530

    Add agent configs.
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

---
+++ internal/config/doc.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+// Package config implements various config structs required in the
+// application.
+//
+// It implements Check that implements the checker.Check and
+// checker.MutableCheck and Component implements the checker.Component
+// interfaces.
+//
+// Also implements the provider interface for metrics.
+//
+// Subpackage configfile includes the configs that are implemented as config
+// files for the application. This includes agents, central server and the
+// app server.
+package config
