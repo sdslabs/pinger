82e3f628f67b5aaaac3909b61a81fd8e6093ad99
Vaibhav <vrongmeal@gmail.com>
2020-05-24 17:25:21 +0530

    Add log metrics exporter.
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

---
+++ internal/metrics/log/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+// Package log implements the log metrics exporter.
+package log
