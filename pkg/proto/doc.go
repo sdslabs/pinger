b3cf5398b9234f4f32960a1f86d56b86fe8cbecf
Vaibhav <vrongmeal@gmail.com>
2020-06-04 00:18:26 +0530

    Add protobufs and generate go protos.
    Also adds make command for building proto.
    
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

---
+++ pkg/proto/doc.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+// Package proto contains the compiled protobuf files.
+//
+// Each file is generated from the proto file contained in
+// the ./protobufs directory. If any proto file is changed,
+// please generate it again using the following command and
+// only then commit your changes:
+//
+// 	$ make proto
+//
+package proto
