8d3bdd785f780b39fd6eb33e8ec52aadfd43b14c
Vaibhav <vrongmeal@gmail.com>
2020-06-06 00:01:57 +0530

    Implement websocket, tcp and udp probers.
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

---
+++ internal/checker/ws/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+// Package ws implements the WS checker and prober.
+package ws
