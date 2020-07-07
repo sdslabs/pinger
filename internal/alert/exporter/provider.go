cbca393f3bbf608a27b770141e5a14547491ce50
shreyaa-sharmaa <shreyasharma.ss305@gmail.com>
2020-07-07 15:26:11 +0530

    linting fixed

--- internal/alert/exporter/provider.go
+++ internal/alert/exporter/provider.go
@@ -4,14 +4,9 @@
 
 package alert
 
-// Alertsystem is anything that can be used to configure and create a alert
+// System is anything that can be used to configure and create a alert
 // exporter.
-type AlertSystem interface {
+type System interface {
 	GetService() string // Returns the service name.
 	GetWebhook() string // Returns the webhook URL of the service.
 }
-
-// similar to inetrnal/exporter/provider we create an interface for storing the webhook url - do we need any other info?
-// declare alert struct in config/ - alert.go
-// should use controller.PullLatestStats for the last state of check if last state is empty
-// should receive its values from internal/agent/agent line 122 - implemented in exporter.go?
