d8f4642165e200bb3f84996bf924538ee435a519
Vaibhav <vrongmeal@gmail.com>
2020-09-08 19:21:55 +0530

    Create docker package.
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

---
+++ pkg/components/app/doc.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 SDSLabs
+// Use of this source code is governed by an MIT license
+// details of which can be found in the LICENSE file.
+
+// Package app defines the app server which is the interface for interacting
+// with the app users.
+package app
