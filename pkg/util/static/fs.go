63a8d363b627f0ccebde887b0189fd50cedc03a7
Vaibhav <vrongmeal@gmail.com>
2020-11-15 04:59:34 +0530

    fix(pkg/util/static): Allow absolute paths.
    This is required in order to fetch from embedded resources. In the
    debugging case, when joining the path, it is solved since joining
    any absolute path with non empty string will be a relative path.
    
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- pkg/util/static/fs.go
+++ pkg/util/static/fs.go
@@ -1,7 +1,6 @@
 package static
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -13,18 +12,22 @@ import (
 
 // baseDir is the directory with the static content. All the static data
 // should be present inside this directory.
+//
+// NB: This should not be an empty string. For current directory, use ".".
 const baseDir = "./static" // assuming commands will be run from root dir.
+
+func init() {
+	if baseDir == "" {
+		panic("pkg/util/static.baseDir cannot be empty string")
+	}
+}
 
 // fsImpl implements the parcello.FileSystemManager interface for actual FS.
 type fsImpl string
 
 // getPath returns the complete path of the file or directory.
-func (f fsImpl) getPath(name string) (string, error) {
-	if filepath.IsAbs(filepath.Clean(name)) {
-		return "", fmt.Errorf("path cannot be absolute: %q", name)
-	}
-
-	return filepath.Join(baseDir, string(f), name), nil
+func (f fsImpl) getPath(name string) string {
+	return filepath.Join(baseDir, string(f), name)
 }
 
 // Open implements the filepath.Open function for http.FileSystem.
@@ -34,21 +37,13 @@ func (f fsImpl) Open(name string) (http.File, error) {
 
 // Walk implements the filepath.Walk function.
 func (f fsImpl) Walk(dir string, fn filepath.WalkFunc) error {
-	actualPath, err := f.getPath(dir)
-	if err != nil {
-		return err
-	}
-
+	actualPath := f.getPath(dir)
 	return filepath.Walk(actualPath, fn)
 }
 
 // OpenFile implements the os.OpenFile method.
 func (f fsImpl) OpenFile(name string, flag int, perm os.FileMode) (parcello.File, error) {
-	actualPath, err := f.getPath(name)
-	if err != nil {
-		return nil, err
-	}
-
+	actualPath := f.getPath(name)
 	return os.OpenFile(actualPath, flag, perm) // nolint:gosec
 }
 
@@ -56,11 +51,6 @@ func (f fsImpl) OpenFile(name string, flag int, perm os.FileMode) (parcello.File
 // environment, i.e., is application running in debug or not.
 func NewFS(ctx *appcontext.Context, path string) (parcello.FileSystem, error) {
 	fpath := filepath.Clean(path)
-	if filepath.IsAbs(fpath) {
-		return nil, fmt.Errorf(
-			"path cannot be absolute; should be relative to static dir: %s", path,
-		)
-	}
 
 	// When running in production, expect the static content packaged inside
 	// the binary, i.e., `resource.go` file to be generated inside this package.
@@ -68,6 +58,8 @@ func NewFS(ctx *appcontext.Context, path string) (parcello.FileSystem, error) {
 	// base directory (to be packaged directory) from the file system when
 	// developing on the application.
 	if !ctx.Debug() {
+		// Make the path absolute for parcello because that's just how it works.
+		fpath = filepath.Join("/", fpath)
 		return parcello.Manager.Dir(fpath)
 	}
 
