f867b61ac474d2fc0103c3b844395b3e14c84e3a
sukhman-sukh <63765293+sukhman-sukh@users.noreply.github.com>
2023-08-29 00:36:06 +0530

    Delete vendor directory

--- vendor/github.com/modern-go/reflect2/go_above_17.go
+++
@@ -1,9 +0,0 @@
-//go:build go1.7
-// +build go1.7
-
-package reflect2
-
-import "unsafe"
-
-//go:linkname resolveTypeOff reflect.resolveTypeOff
-func resolveTypeOff(rtype unsafe.Pointer, off int32) unsafe.Pointer
--- vendor/github.com/sdslabs/kiwi/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/sdslabs/kiwi
-
-go 1.14
-
-require github.com/wangjia184/sortedset v0.0.0-20200422044937-080872f546ba
